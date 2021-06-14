ce08c63c97503f68fe99f802dc3c49fbefd3afb9
Safeer Abbas <safeerabbas3@gmail.com>
2021-06-14 11:01:01 -0400

    Imported Dataset

--- data_processing.py
+++ data_processing.py
@@ -1,8 +1,17 @@
 # ---------------------------------------------------------------------------- #
-#                       Importing the necessary libraries                      #
+#                Importing the necessary libraries and datasets                #
 # ---------------------------------------------------------------------------- #
 
 #Adding a shortcut to save character count
 import numpy as np  
 import pandas as pd
 import matplotlib.pyplot as plt
+
+# Importing Dataset
+
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1 ].values #Taking all the rows, then all the columns excluding the last column
+y = dataset.iloc[:, -1].values #Taking all the rows, and only the last column which Purchased
+print(X, y)
+
+
